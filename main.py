9e04a37fd284927fdf3a46c80612def45762ed8f
Ben Tapscott <bentapscottpro@gmail.com>
2021-07-01 15:11:47 +0200

    add main

--- main.py
+++ main.py
@@ -6,13 +6,26 @@ import streamlit as st
 
 df = pd.read_csv('https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv')
 
+# It is necessary to remove whitespace (and the fullstop)
+
+df['continent'] = df['continent'].str.strip()
+
+df['continent'] = df['continent'].str.replace(".", "")
+
+st.title('Welcome to the car correlation app')
+
+st.sidebar.header('User Input Parameters')
+
+continent = st.sidebar.selectbox('Choose the continent',
+                                 ('US', 'Europe', 'Japan'))
+
 sns.set_theme(style='darkgrid')
 
-# Generate a large random dataset - not used here
-#rs = np.random.RandomState(33)
-
 # Compute the correlation matrix
-corr = df.corr()
+
+df_c = df.loc[(df['continent'] == continent)]
+
+corr = df_c.corr()
 
 # Generate a mask for the upper triangle
 mask = np.triu(np.ones_like(corr, dtype=bool))
@@ -24,8 +37,9 @@ f, ax = plt.subplots(figsize=(20, 15))
 cmap = sns.diverging_palette(230, 20, as_cmap=True)
 
 # Draw the heatmap with the mask and correct aspect ratio
-sns.heatmap(corr, mask=mask, cmap=cmap, annot = True, vmax=.3, center=0, square=True, linewidths=.5, cbar_kws={"shrink": .5})
+sns.heatmap(corr, mask=mask, cmap=cmap, annot=True, vmax=.3, center=0, square=True, linewidths=.5,
+            cbar_kws={"shrink": .5})
 
-plt.title("Diagonal Correlation Matrix 2", fontsize=30)
-#plt.show()
+plt.title("Diagonal Correlation Matrix for your input choice", fontsize=30)
+# plt.show() this is for matplotlib
 st.pyplot(f)
